f15dd443f1fca5d88dbe2e53ca81556ec9e12b5c
peddinti praneeth <praneethpeddinti05@gmail.com>
2025-09-24 11:42:44 +0530

    Update FoodApplication.java

--- src/main/java/com/example/demo/FoodApplication.java
+++ src/main/java/com/example/demo/FoodApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodApplication {
+public class FoodApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodApplication.class, args);
-	}
+    // This ensures Spring Boot runs inside external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FoodApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApplication.class, args);
+    }
 }
